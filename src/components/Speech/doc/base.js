3e59fdf18067eab73b383e9f2b2329032bf49244
zhipenglin <sunandmoon001@163.com>
2024-08-07 20:14:27 +0800

    修改播放音频

--- src/components/Speech/doc/base.js
+++ src/components/Speech/doc/base.js
@@ -1,5 +1,14 @@
-const { default: Speech, AudioRecordButton, AudioPlayerButton } = _Speech;
+const { default: Speech, AudioRecordButton, AudioPlayerButton, useAudioPlayer } = _Speech;
 const { useState } = React;
+const { Button } = antd;
+
+const DurationButton = ({ src }) => {
+  const { getDuration } = useAudioPlayer({ src });
+  return <Button onClick={() => {
+    getDuration().then((time) => console.log(time));
+  }}>获取时长</Button>;
+};
+
 const BaseExample = () => {
   const [record, setRecord] = useState(null);
   return <>
@@ -8,7 +17,10 @@ const BaseExample = () => {
     }} onComplete={(chunks) => {
       setRecord(new Blob(chunks, { type: chunks[0].type }));
     }} />
-    {record && <AudioPlayerButton src={record} />}
+    {record && <>
+      <AudioPlayerButton src={record} />
+      <DurationButton src={record} />
+    </>}
   </>;
 };
 
